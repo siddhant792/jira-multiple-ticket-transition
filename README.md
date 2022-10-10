1edaf1cb7d2941f154066cf30ecbee33d35a2485
Siddhant Gupta <siddhantgupta792000@gmail.com>
2022-10-10 19:04:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Jira Extract Issue Keys
+
+This Github Action parses all the possible issue keys from the commit message.
+
+## Requirements
+
+Issue keys must use capital letters and must have a dash ('-') between the letters and digits. There should be no spaces between the characters of an issue key.
+
+Example: `EXAMPLE-123 TH-1S 1s a commit-1` the only issue key is EXAMPLE-123
+
+## Action Specifications
+
+### Input values
+
+#### Required
+
+Inside your .yml file there should be something that looks like this required variable:
+
+###### Environment variables
+
+```
+env:
+  GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+For more information on Github Environment Variables, see https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
+
+###### Optional variables
+
+```
+uses: HighwayThree/jira-extract-issue-keys@master
+with:
+    is-pull-request: ${{ github.event_name == 'pull_request' }}
+    parse-all-commits: ${{ github.event_name == 'push' }}
+    commit-message: 'EXAMPLE-1 message'
+```
+
+- `is-pull-request` - is true if the GitHub event is a pull request. Default value is false.
+- `parse-all-commits` - is true if the GitHub event is a push. Default value is false.
+- `commit-message` - commit message to be parsed for jira keys. If it is hardcoded like the example above, the smart commits do not send commit information to the hardcoded Jira issues. The default is the commit message, where the smart commits do send information to the issue keys found in the user's commit message.
+
+### Output value
+
+- `jira-keys` - All of the keys found in the commit. 
+    - If is-pull-request was true, then it has all of the keys from all of the commits that were found in the pull request.
+
+## Usage
+
+An example of a pipeline using this action can be found at: 
+> https://github.com/HighwayThree/jira-github-action-integration-demo/blob/master/.github/workflows/ci.yml
---
+++ index.ts
@@ -0,0 +1,109 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const matchAll = require("match-all");
+const Octokit = require("@octokit/rest");
+
+async function extractJiraKeysFromCommit() {
+    try {
+        const regex = /((([A-Z]+)|([0-9]+))+-\d+)/g;
+        const isPullRequest = core.getInput('is-pull-request') == 'true';
+        // console.log("isPullRequest: " + isPullRequest);
+        const commitMessage = core.getInput('commit-message');
+        // console.log("commitMessage: " + commitMessage);
+        // console.log("core.getInput('parse-all-commits'): " + core.getInput('parse-all-commits'));
+        const parseAllCommits = core.getInput('parse-all-commits') == 'true';
+        // console.log("parseAllCommits: " + parseAllCommits);
+        const payload = github.context.payload;
+
+        const token = process.env['GITHUB_TOKEN'];
+        const octokit = new Octokit({
+            auth: token,
+        });
+
+        if (isPullRequest) {
+            let resultArr: any = [];
+
+            // console.log("is pull request...");
+
+            const owner = payload.repository.owner.login;
+            const repo = payload.repository.name;
+            const prNum = payload.number;
+
+            const { data } = await octokit.pulls.listCommits({
+                owner: owner,
+                repo: repo,
+                pull_number: prNum
+            });
+
+            data.forEach((item: any) => {
+                const commit = item.commit;
+                const matches: any = matchAll(commit.message, regex).toArray();
+                matches.forEach((match: any) => {
+                    if (resultArr.find((element: any) => element == match)) {
+                        // console.log(match + " is already included in result array");
+                    } else {
+                        // console.log(" adding " + match + " to result array");
+                        resultArr.push(match);
+                    }
+                });
+
+            });
+
+            const result = resultArr.join(',');
+            core.setOutput("jira-keys", result);
+        }
+        else {
+            // console.log("not a pull request");
+
+            if (commitMessage) {
+                // console.log("commit-message input val provided...");
+                const matches = matchAll(commitMessage, regex).toArray();
+                const result = matches.join(',');
+                core.setOutput("jira-keys", result);
+            }
+            else {
+                // console.log("no commit-message input val provided...");
+                const payload = github.context.payload;
+
+                if (parseAllCommits) {
+                    // console.log("parse-all-commits input val is true");
+                    let resultArr: any = [];
+
+                    payload.commits.forEach((commit: any) => {
+                        const matches = matchAll(commit.message, regex).toArray();
+                        matches.forEach((match: any) => {
+                            if (resultArr.find((element: any) => element == match)) {
+                                // console.log(match + " is already included in result array");
+                            } else {
+                                // console.log(" adding " + match + " to result array");
+                                resultArr.push(match);
+                            }
+                        });
+
+                    });
+
+                    const result = resultArr.join(',');
+                    core.setOutput("jira-keys", result);
+                }
+                else {
+                    // console.log("parse-all-commits input val is false");
+                    // console.log("head_commit: ", payload.head_commit);
+                    const matches = matchAll(payload.head_commit.message, regex).toArray();
+                    const result = matches.join(',');
+                    core.setOutput("jira-keys", result);
+                }
+
+            }
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+(async function () {
+    await extractJiraKeysFromCommit();
+    // console.log("finished extracting jira keys from commit message");
+})();
+
+export default extractJiraKeysFromCommit
